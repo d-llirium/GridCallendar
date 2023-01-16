fb8e45e75bc7379a225a3110fb88cb41700d4418
plimacarlos <plimacarlos@brq.com>
2023-01-16 17:04:29 -0300

    Make the grid scroll

--- GridCallendar/ContentView.swift
+++ GridCallendar/ContentView.swift
@@ -14,18 +14,21 @@ struct ContentView: View
     ]
     var body: some View
     {
-        LazyVGrid( columns: layout ) // Lazy in LazyVGrid and LazyHGrid refers to the fact that the elements of the grid aren’t created until they are needed to display in the view
+        ScrollView
         {
-            ForEach(
-                year[ 0 ].days //  Month of January
-            ) { day in // which is an array of Day instances
-                Capsule() // we create a Capsule with overlay text content that displays a string equal to the value property of the Day.
-                    .overlay(
-                        Text("\(day.value)")
-                            .foregroundColor(.white)
-                    )
-                    .foregroundColor(.blue)
-                    .frame(height: 40)
+            LazyVGrid( columns: layout ) // Lazy in LazyVGrid and LazyHGrid refers to the fact that the elements of the grid aren’t created until they are needed to display in the view
+            {
+                ForEach(
+                    year[ 0 ].days //  Month of January
+                ) { day in // which is an array of Day instances
+                    Capsule() // we create a Capsule with overlay text content that displays a string equal to the value property of the Day.
+                        .overlay(
+                            Text("\(day.value)")
+                                .foregroundColor(.white)
+                        )
+                        .foregroundColor(.blue)
+                        .frame(height: 40)
+                }
             }
         }
     }
@@ -40,7 +43,7 @@ struct Month
     let name: String
     let numberOfDays: Int
     var days: [ Day ]
- 
+    
     init(
         name: String,
         numberOfDays: Int
@@ -48,7 +51,7 @@ struct Month
         self.name = name
         self.numberOfDays = numberOfDays
         self.days = []
- 
+        
         for n in 1...numberOfDays
         {
             self.days.append(
@@ -58,19 +61,19 @@ struct Month
     }
 }
 let year = [
-        Month(name: "January", numberOfDays: 31),
-        Month(name: "February", numberOfDays: 28),
-        Month(name: "March", numberOfDays: 31),
-        Month(name: "April", numberOfDays: 30),
-        Month(name: "May", numberOfDays: 31),
-        Month(name: "June", numberOfDays: 30),
-        Month(name: "July", numberOfDays: 31),
-        Month(name: "August", numberOfDays: 31),
-        Month(name: "September", numberOfDays: 30),
-        Month(name: "October", numberOfDays: 31),
-        Month(name: "November", numberOfDays: 30),
-        Month(name: "December", numberOfDays: 31),
-    ]
+    Month(name: "January", numberOfDays: 31),
+    Month(name: "February", numberOfDays: 28),
+    Month(name: "March", numberOfDays: 31),
+    Month(name: "April", numberOfDays: 30),
+    Month(name: "May", numberOfDays: 31),
+    Month(name: "June", numberOfDays: 30),
+    Month(name: "July", numberOfDays: 31),
+    Month(name: "August", numberOfDays: 31),
+    Month(name: "September", numberOfDays: 30),
+    Month(name: "October", numberOfDays: 31),
+    Month(name: "November", numberOfDays: 30),
+    Month(name: "December", numberOfDays: 31),
+]
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
