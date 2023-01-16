9dc737d7e4057f68a76cfff94f23300176947600
plimacarlos <plimacarlos@brq.com>
2023-01-16 17:01:43 -0300

    create GridItem and LazyVGrid

--- GridCallendar/ContentView.swift
+++ GridCallendar/ContentView.swift
@@ -9,10 +9,25 @@ import SwiftUI
 
 struct ContentView: View
 {
+    let layout = [
+        GridItem( .fixed(40) ) // a single fixed column of width 40
+    ]
     var body: some View
     {
-        Text("Hello, world!")
-            .padding()
+        LazyVGrid( columns: layout ) // Lazy in LazyVGrid and LazyHGrid refers to the fact that the elements of the grid aren’t created until they are needed to display in the view
+        {
+            ForEach(
+                year[ 0 ].days //  Month of January
+            ) { day in // which is an array of Day instances
+                Capsule() // we create a Capsule with overlay text content that displays a string equal to the value property of the Day.
+                    .overlay(
+                        Text("\(day.value)")
+                            .foregroundColor(.white)
+                    )
+                    .foregroundColor(.blue)
+                    .frame(height: 40)
+            }
+        }
     }
 }
 struct Day: Identifiable //  Identifiable prot requiresan id value that is unique
