debda46b5f4257bd0c4d45b5bce0e20662725583
plimacarlos <plimacarlos@brq.com>
2023-01-16 16:38:58 -0300

    Initial Commit

---
+++ GridCallendar/GridCallendarApp.swift
@@ -0,0 +1,17 @@
+////  GridCallendarApp.swift
+//  GridCallendar
+//
+//  Created by Patricia Carlos on 16/01/23.
+//  
+//
+
+import SwiftUI
+
+@main
+struct GridCallendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
