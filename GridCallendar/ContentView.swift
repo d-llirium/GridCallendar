083a8a3d0f3ee1f40b7d8eb8b1f6c4e5d7fa035e
plimacarlos <plimacarlos@brq.com>
2023-01-16 17:26:23 -0300

    Adding sections to the grid one for each month

--- GridCallendar/ContentView.swift
+++ GridCallendar/ContentView.swift
@@ -10,10 +10,15 @@ import SwiftUI
 struct ContentView: View
 {
     let layout = [
-        GridItem( .fixed(40) ), // a single fixed column of width 40
-        GridItem( .fixed(40) ),
-        GridItem( .flexible(minimum: 40) ),
-        GridItem( .flexible(minimum: 40) )
+//        GridItem( .fixed(40) ), // a single fixed column of width 40
+        GridItem( .flexible( minimum: 40) ),
+        GridItem( .flexible( minimum: 40 ) ),
+        GridItem( .flexible( minimum: 40 ) ),
+        GridItem( .flexible( minimum: 40 ) ),
+        GridItem( .flexible( minimum: 40 ) ),
+        GridItem( .flexible( minimum: 40 ) ),
+        GridItem( .flexible( minimum: 40 ) ) //  flexible option creates a column that takes up the available width it has. You must define a minimum width, and you can optionally specify a maximum width
+//        GridItem( .adaptive(minimum: 80) ) // adaptive option fits multiple columns inside the width of a single GridItem
     ]
     var body: some View
     {
@@ -22,16 +27,26 @@ struct ContentView: View
             LazyVGrid( columns: layout ) // Lazy in LazyVGrid and LazyHGrid refers to the fact that the elements of the grid aren’t created until they are needed to display in the view
             {
                 ForEach(
-                    year[ 0 ].days //  Month of January
-                ) { day in // which is an array of Day instances
-                    Capsule() // we create a Capsule with overlay text content that displays a string equal to the value property of the Day.
-                        .overlay(
-                            Text("\(day.value)")
-                                .foregroundColor(.white)
-                        )
-                        .foregroundColor(.blue)
-                        .frame(height: 40)
+                    year, id: \.name  //  iterates through all of the months of the year. we didn’t make the Month structure conform to Identifiable, so we need to specify a unique key path in the ForEach structure. As long as we don’t have the same name of the month in our year, we can use it as the unique id
+                ) { month in
+                    Section(
+                        header: Text( verbatim: month.name ) // name of the month
+                            .font( .headline )
+                    ) {
+                        ForEach(
+                           month.days // days of that month
+                        ) { day in // which is an array of Day instances
+                            Capsule() // we create a Capsule with overlay text content that displays a string equal to the value property of the Day.
+                                .overlay(
+                                    Text("\(day.value)")
+                                        .foregroundColor(.white)
+                                )
+                                .foregroundColor(.blue)
+                                .frame(height: 40)
+                        }
+                    }
                 }
+                
             }
         }
     }
